2a4db2e57f71a5024344a02bb5fd9bfcdba5eb64
victorduun <43340964+victorduun@users.noreply.github.com>
2020-03-21 21:47:46 +0100

    Update gui to allow selecting municipality & simple progress display

--- BoligsidenScraper/MainWindow.xaml.cs
+++ BoligsidenScraper/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using BoligsidenScraper.Scrapers.Searchsite;
 using Newtonsoft.Json;
-
+using System.Windows.Controls;
 namespace BoligsidenScraper
 {
     /// <summary>
@@ -16,43 +16,88 @@ namespace BoligsidenScraper
     /// </summary>
     public partial class MainWindow : Window
     {
+           
+        public class TextboxText
+        {
+            public string textdata { get; set; }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            InfoText.DataContext = new TextboxText() { textdata = JsonDirectory + "\\[Municipality].json" };
+            this.Loaded += MainWindow_Loaded;
         }
-        public static string AssemblyDirectory
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> options = new List<string>();
+            options.Add("All");
+            foreach (var option in Municipalities.MunicipalityCollection)
+                options.Add(option);
+            cmbMunicipalityList.ItemsSource = options;
+            cmbMunicipalityList.Text = "Choose municipality.";
+        }
+
+        public static string JsonDirectory
         {
             get
             {
                 string codeBase = Assembly.GetExecutingAssembly().CodeBase;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path);
+                return Path.GetDirectoryName(path) + "\\kommuner";
             }
         }
 
+
+
         private async void On_Button_Run_Clicked(object sender, RoutedEventArgs e)
         {
-            var municipalities = Municipalities.MunicipalityCollection;
 
-            foreach(string municipality in municipalities)
+            Directory.CreateDirectory(JsonDirectory);
+
+            List<string> municipalitiesToCheck = new List<string>();
+
+            string selectedItem = Convert.ToString(cmbMunicipalityList.SelectedItem);
+            //Check if valid municipality
+            if(selectedItem == "All")
+                municipalitiesToCheck = Municipalities.MunicipalityCollection.ToList();
+            else if(String.IsNullOrEmpty(selectedItem))
             {
+                MessageBox.Show("No municipality selected.", "Error");
+                return;
+            }
+            else
+            {
+                if (Municipalities.MunicipalityCollection.Contains(selectedItem))
+                    municipalitiesToCheck.Add(selectedItem);
+                else
+                {
+                    MessageBox.Show("Invalid municipality.", "Error");
+                    return;
+                }
+            }
+
+            Run.IsEnabled = false;
+
+            foreach (string municipality in municipalitiesToCheck)
+            {
+                ProgressTextBox.Text = "Scraping data for municipality: " + municipality;
                 var handler = new SearchSiteHandler(municipality);
 
                 List<PropertySale> sales = await handler.GetAllPropertySales();
-                
+
                 //Turn to json string
                 string json = JsonConvert.SerializeObject(sales.ToArray(),Formatting.Indented);
 
                 //Write json to file
-                string fileDirectory = AssemblyDirectory + "\\kommuner";
-                string completeFilePath = fileDirectory + "\\" + municipality + ".json";
-                Directory.CreateDirectory(fileDirectory);
+                string completeFilePath = JsonDirectory + "\\" + municipality + ".json";
                 File.WriteAllText(completeFilePath, json);
             }
 
-        
-
+            Run.IsEnabled = true;
+            ProgressTextBox.Text = "Ready to start data scraping.";
+            MessageBox.Show("Finished scraping data for municipality: " + selectedItem, "Complete");
 
         }
     }
